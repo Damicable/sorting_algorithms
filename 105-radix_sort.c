530da6b8a414aeddba44dec0191fea05b28b51ce
Damicable <annobidiegwu@gmail.com>
2023-07-25 16:53:34 -0700

    Corrected sorthing algorithm

---
+++ 105-radix_sort.c
@@ -0,0 +1,60 @@
+#include "sort.h"
+#include "102-counting_sort.c"
+/**
+ * counting_s - This function handles the counting sort based on the
+ * significant digit.
+ * @array: A pointer to the array of integers.
+ * @size: Size of the array.
+ * @exp: Current significant digit for counting sort.
+ */
+void counting_s(int *array, size_t size, int exp)
+{
+	int *count_s = malloc(size * sizeof(int)), bins[10], j;
+	size_t i;
+
+	for (j = 0; j < 10; j++)
+		bins[j] = 0;
+
+	if (count_s == NULL)
+		return;
+
+	for (i = 0; i < size; ++i)
+		bins[(array[i] / exp) % 10]++;
+
+	for (j = 1; j < 10; ++j)
+		bins[j] += bins[j - 1];
+
+	for (j = size - 1; j >= 0; --j)
+	{
+		count_s[bins[(array[j] / exp) % 10] - 1] = array[j];
+		bins[(array[j] / exp) % 10]--;
+	}
+
+	for (i = 0; i < size; ++i)
+		array[i] = count_s[i];
+
+	free(count_s);
+}
+
+/**
+ * radix_sort - This function sorts an array of integers in ascending
+ * order using Radix sort.
+ * @array: A pointer to the array of integers to be sorted.
+ * @size: Size of the array.
+ */
+void radix_sort(int *array, size_t size)
+{
+
+	int exp, max;
+
+	if (!array || size < 2)
+		return;
+
+ 	max = maximum(array, size);
+
+	for (exp = 1; max / exp > 0; exp *= 10)
+	{
+		counting_s(array, size, exp);
+		print_array(array, size);
+	}
+}
